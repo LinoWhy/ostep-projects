cf2700cc136e71d7b393ccd56c35a6a6d97f8e1e
LinoWhy <yuan.lijun@hotmail.com>
2022-03-20 13:11:30 +0800

    initial | count SYS_read with spinlock

--- initial-xv6/src/sysfile.c
+++ initial-xv6/src/sysfile.c
@@ -16,6 +16,9 @@
 #include "file.h"
 #include "fcntl.h"
 
+unsigned int readcount = 0;
+struct spinlock readlock;
+
 // Fetch the nth word-sized system call argument as a file descriptor
 // and return both the descriptor and the corresponding struct file.
 static int
@@ -73,9 +76,18 @@ sys_read(void)
   int n;
   char *p;
 
+  acquire(&readlock);
+  readcount++;
+  release(&readlock);
+
   if(argfd(0, 0, &f) < 0 || argint(2, &n) < 0 || argptr(1, &p, n) < 0)
     return -1;
   return fileread(f, p, n);
+}
+
+int sys_getreadcount(void)
+{
+  return readcount;
 }
 
 int
@@ -374,7 +386,7 @@ sys_chdir(void)
   char *path;
   struct inode *ip;
   struct proc *curproc = myproc();
-  
+
   begin_op();
   if(argstr(0, &path) < 0 || (ip = namei(path)) == 0){
     end_op();
